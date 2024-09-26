6db9ef536cc5329b1ef1aac252cc8eefe2afbe70
Luiz - Personal <luizgmaker@gmail.com>
2024-09-26 20:36:04 -0300

    added eks node groups definition

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -9,3 +9,21 @@ resource "aws_eks_cluster" "tech-challenge-cluster" {
     security_group_ids = [aws_security_group.eks_sg.id]
   }
 }
+
+resource "aws_eks_node_group" "managed_nodes" {
+  cluster_name    = aws_eks_cluster.tech-challenge-cluster.name
+  node_group_name = "main-node-group"
+  node_role_arn   = var.aws_iam_labrole_arn
+  subnet_ids      = aws_subnet.private_subnet[*].id
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+  instance_types = ["t3.medium"]  # Tipo de instância dos nós
+  disk_size      = 20            # Tamanho do disco dos nós (em GB)
+
+  tags = {
+    Name = "main-node-group"
+  }
+}
